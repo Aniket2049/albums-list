302aac0f16b40b37be2fd08439ecff4d538de07a
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-12-16 22:42:33 +0530

    rename album property to title in data

--- src/data.js
+++ src/data.js
@@ -2,25 +2,25 @@ const list = [
   {
     id: 1,
     artist: "Timecop1983",
-    album: "Night Drive",
+    title: "Night Drive",
     albumArtURL: "https://f4.bcbits.com/img/a2412369100_16.jpg",
   },
   {
     id: 2,
     artist: "The Midnight",
-    album: "Endless Summer",
+    title: "Endless Summer",
     albumArtURL: "https://f4.bcbits.com/img/a2817758500_16.jpg",
   },
   {
     id: 3,
     artist: "W O L F C L U B",
-    album: "Infinity",
+    title: "Infinity",
     albumArtURL: "https://f4.bcbits.com/img/a2399015455_16.jpg",
   },
   {
     id: 4,
     artist: "New Arcades",
-    album: "We Had It All For Just A Moment",
+    title: "We Had It All For Just A Moment",
     albumArtURL: "https://f4.bcbits.com/img/a1416337527_16.jpg",
   },
 ];
