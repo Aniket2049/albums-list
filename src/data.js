4712e61ef505e3ae8fd4e5ec4a8fbb25ea96a747
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-12-16 22:38:11 +0530

    create data file

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const list = [
+  {
+    id: 1,
+    artist: "Timecop1983",
+    album: "Night Drive",
+    albumArtURL: "https://f4.bcbits.com/img/a2412369100_16.jpg",
+  },
+  {
+    id: 2,
+    artist: "The Midnight",
+    album: "Endless Summer",
+    albumArtURL: "https://f4.bcbits.com/img/a2817758500_16.jpg",
+  },
+  {
+    id: 3,
+    artist: "W O L F C L U B",
+    album: "Infinity",
+    albumArtURL: "https://f4.bcbits.com/img/a2399015455_16.jpg",
+  },
+  {
+    id: 4,
+    artist: "New Arcades",
+    album: "We Had It All For Just A Moment",
+    albumArtURL: "https://f4.bcbits.com/img/a1416337527_16.jpg",
+  },
+];
+
+export default list;
