bde879f649d7e13864cdc2c2ae06ca5fbc335aaa
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-12-16 23:16:50 +0530

    add conditional rendering

--- src/List.js
+++ src/List.js
@@ -17,22 +17,34 @@ const List = () => {
 
   return (
     <div className='list'>
-      {albums.map((album) => {
-        return (
-          <div key={album.id} className='list-item'>
-            <img
-              src={album.albumArtURL}
-              alt={album.title}
-              width='100px'
-              height='100px'
-            />
-            <div style={{ flexGrow: "1" }}>
-              <div className='artist'>{album.artist}</div>
-              <div className='album'>{album.title}</div>
+      {albums.length > 0 ? (
+        albums.map((album) => {
+          return (
+            <div key={album.id} className='list-item'>
+              <img
+                src={album.albumArtURL}
+                alt={album.title}
+                width='100px'
+                height='100px'
+              />
+              <div style={{ flexGrow: "1" }}>
+                <div className='artist'>{album.artist}</div>
+                <div className='album'>{album.title}</div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <p
+          style={{
+            fontFamily: "Barlow",
+            fontSize: "larger",
+            fontWeight: "bold",
+          }}
+        >
+          No Music 😟
+        </p>
+      )}
       <button className='clearListItemsBtn' onClick={clearListItemsHandler}>
         Clear All
       </button>
